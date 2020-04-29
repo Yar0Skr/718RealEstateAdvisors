a793feb5c56d184407dfe7ca865563ab297e2e74
Yaroslav Skrypnyk <yar.skrypnyk@gmail.com>
2020-04-29 15:26:02 +0300

    "Front"

--- views/site/index.php
+++ views/site/index.php
@@ -1,5 +1,6 @@
 <?php
 use kv4nt\owlcarousel\OwlCarouselWidget;
+use yii\helpers\Html;
 use yii\helpers\Url;
 
 /* @var $this yii\web\View */
@@ -22,10 +23,10 @@ $this->title = '718 Real Estate Advisors';
     ]);
     ?>
 
-    <div class="item-class"><img src="/img/layouts/header.png" alt="Image 1"></div>
-    <div class="item-class"><img src="/img/2.jpg" alt="Image 2"></div>
-    <div class="item-class"><img src="/img/3.jpg" alt="Image 3"></div>
-    <div class="item-class"><img src="/img/4.jpg" alt="Image 4"></div>
+<div class="item-class"><img src="/img/layouts/header.png" alt="Image 1"></div>
+<div class="item-class"><img src="/img/layouts/header.png" alt="Image 1"></div>
+<div class="item-class"><img src="/img/layouts/header.png" alt="Image 1"></div>
+<div class="item-class"><img src="/img/layouts/header.png" alt="Image 1"></div>
     <?php OwlCarouselWidget::end(); ?>
 
 
@@ -34,12 +35,19 @@ $this->title = '718 Real Estate Advisors';
         <div class="flex-div"><h2>Watch this short video to learn about us</h2></div>
         <div class="flex-div"><h2>Are you looking to sell house fast?</h2></div>
     </div>
-    <div class="container container-flex">
-        <div class="flex-div"></div>
-        <div class="flex-div">
+    <div class="container container-flex bottom-flex">
+        <div class="flex-div index-top">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Pq8lUumT-mE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class="flex-div bottom-flex">
             <p>Whether your property is vacant or rented to tenants, paying or non-paying, and whether your house is distressed or in excellent condition.</p>
             <br>
             <p>We accommodate all your needs and make the process comfortable for you.</p>
+            <br>
+            <br>
+            <a href="tel:6462879111">
+                <button class="btn send-btn">Call us for a fair all-cash offer</button>
+            </a>
         </div>
     </div>
 </div>
@@ -52,9 +60,11 @@ $this->title = '718 Real Estate Advisors';
         </div>
     </div>
     <div class="container container-flex">
-        <div class="flex-div"></div>
-        <div class="flex-div">
-            <ul style="list-style: none">
+        <div class="flex-div bottom-flex index-middle">
+
+        </div>
+        <div class="flex-div bottom-flex">
+            <ul style=" padding: 0;list-style: none">
                 <li>- Single Family</li>
                 <li>- Multi Family</li>
                 <li>- Cooperatives</li>
@@ -65,14 +75,21 @@ $this->title = '718 Real Estate Advisors';
                 <li>WE BUY THEM ALL</li>
                 <li>Anywhere in New York and New Jersey</li>
             </ul>
+            <br>
+            <br>
+            <a href="tel:6462879111">
+                <button class="btn send-btn">Call or fill out the Form</button>
+            </a>
         </div>
     </div>
 </div>
 
 <div class="content gray">
     <div class="container container-flex">
-        <div class="flex-div"></div>
-        <div class="flex-div">
+        <div class="flex-div bottom-flex index-bottom">
+
+        </div>
+        <div class="flex-div bottom-flex">
             <h2>Avoid Foreclosure</h2>
             <br>
             <p>We can help if your property is distressed and we can assist you with foreclosure solutions.</p>
