c3c95987ca67d79e411fed43299179474299339e
Yar0Skr <yar.skrypnyk@gmail.com>
2020-06-15 07:17:48 -0600

    Left side for nav

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,7 +21,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/right-nav-style.css'
+        'css/left-nav-style.css'
     ];
     public $js = [
         'js/script.js'
