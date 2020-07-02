2cd72199ea310af087d66badbfbe9493a3246f6d
Yaroslav Skrypnyk <yar.skrypnyk@gmail.com>
2020-07-02 15:39:48 +0300

    "undo"

--- views/sell-house-fast/reasons.php
+++ views/sell-house-fast/reasons.php
@@ -36,93 +36,93 @@ $this->params['breadcrumbs'][] = $this->title;
         ?>
         
         <div class="item-class"><a href="<?=Url::toRoute('blog/selling-home-with-hpd-violations')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/HPD Building Department Violations.jpg",
-                    'options'=>['alt' => 'HPD Building Department Violations']])?></a></div>
+                <?=Html::img("/web/img/layouts/HPD Building Department Violations.jpg",
+                ['alt' => 'HPD Building Department Violations'])?></a></div>
         
         <div class="item-class"><a href="<?=Url::toRoute('blog/the-ideal-strategy-to-deal-with-hpd-alternative-enforcement-program')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/AlternativeEnforcementProgram-min.jpg",
-                    'options'=>['alt' => 'Alternative Enforcement Program']])?></a></div>
+                <?=Html::img("/web/img/layouts/AlternativeEnforcementProgram-min.jpg",
+                ['alt' => 'Alternative Enforcement Program'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/lost-my-job-cant-pay-mortgage')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Can't Afford Mortgage Payments.jpg",
-                    'options'=>['alt' => "Can't Afford Mortgage Payments"]])?></a></div>
+                <?=Html::img("/web/img/layouts/Can't Afford Mortgage Payments.jpg",
+                ['alt' => "Can't Afford Mortgage Payments"])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/sell-house-fast-divorce')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Divorce.jpg",
-                    'options'=>['alt' => 'Divorce']])?></a></div>
+                <?=Html::img("/web/img/layouts/Divorce.jpg",
+                ['alt' => 'Divorce'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/dissolving-real-estate-partnership')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Dysfunctional Partnership.jpg",
-                    'options'=>['alt' => 'Dysfunctional Partnership']])?></a></div>
+                <?=Html::img("/web/img/layouts/Dysfunctional Partnership.jpg",
+                ['alt' => 'Dysfunctional Partnership'])?></a></div>
         
         <div class="item-class"><a href="<?=Url::toRoute('blog/selling-house-that-needs-repairs')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Emergency Repairs.jpg",
-                    'options'=>['alt' => 'Emergency Repairs']])?></a></div>
+                <?=Html::img("/web/img/layouts/Emergency Repairs.jpg",
+                ['alt' => 'Emergency Repairs'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/selling-inherited-property')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/EstateProbateInheritance.jpg",
-                    'options'=>['alt' => 'Estate Probate Inheritance']])?></a></div>
+                <?=Html::img("/web/img/layouts/EstateProbateInheritance.jpg",
+                ['alt' => 'Estate Probate Inheritance'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/selling-fire-damaged-house')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/FireWater Damage.jpg",
-                    'options'=>['alt' => 'Fire/Water Damage']])?></a></div>
+                <?=Html::img("/web/img/layouts/FireWater Damage.jpg",
+                ['alt' => 'Fire/Water Damage'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/faced-with-a-foreclosure')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Foreclosure.jpg",
-                    'options'=>['alt' => 'Foreclosure']])?></a></div>
+                <?=Html::img("/web/img/layouts/Foreclosure.jpg",
+                ['alt' => 'Foreclosure'])?></a></div>
                 
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/struggling-with-guardianship')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Guardianship.jpg",
-                    'options'=>['alt' => 'Guardianship']])?></a></div>
+                <?=Html::img("/web/img/layouts/Guardianship.jpg",
+                ['alt' => 'Guardianship'])?></a></div>
                 
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/how-to-sell-an-ugly-house')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/HPD Zombie Homes Initiative.jpg", 'options'=>['alt' => 'HPD Zombie Homes Initiative']])?></a></div>
+                <?=Html::img("/web/img/layouts/HPD Zombie Homes Initiative.jpg", ['alt' => 'HPD Zombie Homes Initiative'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/dealing-with-credit-judgments')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Judgement.jpg",
-                    'options'=>['alt' => 'Judgement']])?></a></div>
+                <?=Html::img("/web/img/layouts/Judgement.jpg",
+                ['alt' => 'Judgement'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/need-to-sell-house-quickly')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Need to Sell Quickly for Personal Reasons.jpg",
-                    'options'=>['alt' => 'Need to Sell Quickly for Personal Reasons']])?></a></div>
+                <?=Html::img("/web/img/layouts/Need to Sell Quickly for Personal Reasons.jpg",
+                ['alt' => 'Need to Sell Quickly for Personal Reasons'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/is-your-home-mortgage-underwater')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Negative Equity.jpg",
-                    'options'=>['alt' => 'Negative Equity']])?></a></div>
+                <?=Html::img("/web/img/layouts/Negative Equity.jpg",
+                ['alt' => 'Negative Equity'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/dealing-with-problem-tenants')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Problem Tenants.jpg",
-                    'options'=>['alt' => 'Problem Tenants']])?></a></div>
+                <?=Html::img("/web/img/layouts/Problem Tenants.jpg",
+                ['alt' => 'Problem Tenants'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/selling-house-in-disrepair')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Property In Disrepair.jpg",
-                    'options'=>['alt' => 'Property In Disrepair']])?></a></div>
+                <?=Html::img("/web/img/layouts/Property In Disrepair.jpg",
+                ['alt' => 'Property In Disrepair'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/sell-house-fast-from-distance')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Relocation.jpg",
-                    'options'=>['alt' => 'Relocation']])?></a></div>
+                <?=Html::img("/web/img/layouts/Relocation.jpg",
+                ['alt' => 'Relocation'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/how-to-sell-house-with-reverse-mortgage')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Reverse Mortgage.jpg",
-                    'options'=>['alt' => 'Reverse Mortgage']])?></a></div>
+                <?=Html::img("/web/img/layouts/Reverse Mortgage.jpg",
+                ['alt' => 'Reverse Mortgage'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/short-sale-avoid-foreclosure')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Short Sale.jpg",
-                    'options'=>['alt' => 'Short Sale']])?></a></div>
+                <?=Html::img("/web/img/layouts/Short Sale.jpg",
+                ['alt' => 'Short Sale'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/troubled-investment-property-what-is-the-best-solution')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Troubled Investment Property.jpg",
-                    'options'=>['alt' => 'Troubled Investment Property']])?></a></div>
+                <?=Html::img("/web/img/layouts/Troubled Investment Property.jpg",
+                ['alt' => 'Troubled Investment Property'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/unwanted-property-selling-house-left-in-will')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Unwanted Property On Your Name.jpg",
-                    'options'=>['alt' => 'Unwanted Property On Your Name']])?></a></div>
+                <?=Html::img("/web/img/layouts/Unwanted Property On Your Name.jpg",
+                ['alt' => 'Unwanted Property On Your Name'])?></a></div>
                 
         <div class="item-class"><a href="<?=Url::toRoute('blog/facing-auction-sale-due-to-mortgage-foreclosure')?>">
-                <?=LazyLoad::widget(['src'=>"/web/img/layouts/Upcoming Auction.jpg",
-                    'options'=>['alt' => 'Upcoming Auction']])?></a></div>
+                <?=Html::img("/web/img/layouts/Upcoming Auction.jpg",
+                ['alt' => 'Upcoming Auction'])?></a></div>
 
 
 
