93c96623f3eb8ee5c5a68961d5120acbc376d9e4
Yaroslav Skrypnyk <yar.skrypnyk@gmail.com>
2020-07-20 16:20:23 +0300

    "FB"

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -59,6 +59,30 @@ AppAsset::register($this);
 <body>
 
 <?php $this->beginBody() ?>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v7.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="109093680591201"
+     theme_color="#0084ff">
+</div>
 <div class = "pre-nav">
     <div class="flex-nav">
         <a href=<?=Url::home()?>><?=Html::img("/web/img/icons/logo.png",['alt' => "Logo"])?></a>
